f3fa88fc7e9b90e3426057c58d89489a47768094
Vladislav Alexeyev <vlad@alexeyev.me>
2015-05-05 00:48:54 +0300

    App bootstrap

---
+++ js/storage.js
@@ -0,0 +1,7 @@
+var storage = {
+    // `sync` if you want. remember about QUOTAS https://developer.chrome.com/extensions/storage.html#sync-properties
+    area: chrome.storage.local,
+    default_options: {
+        // one key for every option
+    }
+};
