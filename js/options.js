24a00c75b0cbf720e984be36404fe0c857e9ac50
Ilya Chekalskiy <ilya@chekalskiy.ru>
2015-05-05 13:36:44 +0300

    Убрал всё лишнее, что нам не понадобится в этой задаче

--- js/options.js
+++
@@ -1,5 +0,0 @@
-var main_tabs = new Tabs('#main_tabs');
-
-document.addEventListener('optionsPageReady', function () {
-    /* Options page is ready. Write your code here */
-});
--- js/storage.js
+++
@@ -1,7 +0,0 @@
-var storage = {
-    // `sync` if you want. remember about QUOTAS https://developer.chrome.com/extensions/storage.html#sync-properties
-    area: chrome.storage.local,
-    default_options: {
-        // one key for every option
-    }
-};
